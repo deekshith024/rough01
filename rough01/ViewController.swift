9ef17bee8a91d65bd7808ffaff2a6b30bbec73d3
deekshith024 <deekshithreddy0001@gmail.com>
2022-07-19 14:08:42 -0400

    Initial Commit

---
+++ rough01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rough01
+//
+//  Created by Karthik Reddy Rondla on 2022-07-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
